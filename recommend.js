2f60feffe4311de9b868262017532f42118a56a8
Yosoichi <49706857+Yosoichi@users.noreply.github.com>
2019-05-09 18:27:25 +0900

    Update recommend.js
    順位を2019年5月現在のものに

--- recommend.js
+++ recommend.js
@@ -2,17 +2,17 @@
 var grade=rec.getElementsByClassName("grade");
 var urls=[
     "https://sksksketch.net/original/20181225.html",
-    "https://sksksketch.net/original/20181030.html",
-    "https://sksksketch.net/funart/20181214.html",
+    "https://sksksketch.net/funart/20190506.html",
     "https://sksksketch.net/funart/20170728.html",
-    "https://sksksketch.net/1d-1sketch/20160716.html"
+    "https://sksksketch.net/1d-1sketch/20160716.html",
+    "https://sksksketch.net/1d-1sketch/20160915.html"
 ];
 var imgs=[
     "https://sksksketch.net/images/recommends/1st.jpg",
     "https://sksksketch.net/images/recommends/2nd.jpg",
-    "https://sksksketch.net/images/recommends/3rd.jpg",
+    "https://sksksketch.net/images/recommends/3rd.png",
     "https://sksksketch.net/images/recommends/4th.png",
-    "https://sksksketch.net/images/recommends/5th.png"
+    "https://sksksketch.net/images/recommends/5th.jpg"
 ];
 
 for(var i=0;i<grade.length;i++){
